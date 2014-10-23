87c9fce2c64b9eff58f920fd2b733d4f70c78bfb
Rounak <rounak91@gmail.com>
2014-10-23 21:02:01 +0530

    - added button refrencing outlets

--- CircleTransition/ViewController.swift
+++ CircleTransition/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var button: UIButton!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
