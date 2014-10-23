c7d1883bd3ad417493861d43660136372ef31366
Rounak <rounak91@gmail.com>
2014-10-24 02:00:03 +0530

    reindent

--- CircleTransition/ViewController.swift
+++ CircleTransition/ViewController.swift
@@ -9,23 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
   @IBOutlet weak var button: UIButton!
-
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func circleTapped(sender:UIButton) {
-        self.navigationController?.popViewControllerAnimated(true)
-    }
-
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  @IBAction func circleTapped(sender:UIButton) {
+    self.navigationController?.popViewControllerAnimated(true)
+  }
+  
 }
 
